197eff1552af1a66f6177849076ebdf44432faf1
Falco Barstoff <36726286+barstoff@users.noreply.github.com>
2020-05-22 16:47:46 +0200

    Update sankey_plot.R

--- R_code/plots/sankey_plot.R
+++ R_code/plots/sankey_plot.R
@@ -2,25 +2,80 @@ rm(list=ls()) # to clean the memeory
 library(haven)
 library(Gmisc)
 library(RColorBrewer)
+library(xtable)
 
 setwd("G:\\Il mio Drive\\Research\\Italian Firms\\Zombie Hunting New Data")
 data <- read_dta("zombie_data.dta")
 data_italy <- data[which(data$iso=="IT"),] 
 
+data_italy$transition_2009 <- data_italy$cat_2009
+data_italy$transition_2009[which(data_italy$transition_2009==0)] <- 5
+data_italy$transition_2009[which(data_italy$transition_2009==4)][1:2434] <- 3
+data_italy$transition_2009[which(data_italy$transition_2009==5)][1:6059] <- 3
 data_italy$transition_2010 <- data_italy$cat_2010
 data_italy$transition_2010[which(data_italy$transition_2010==0)] <- 5
 data_italy$transition_2011 <- data_italy$cat_2011
 data_italy$transition_2011[which(data_italy$transition_2011==0)] <- 5
+data_italy$transition_2012 <- data_italy$cat_2012
+data_italy$transition_2012[which(data_italy$transition_2012==0)] <- 5
+data_italy$transition_2013 <- data_italy$cat_2013
+data_italy$transition_2013[which(data_italy$transition_2013==0)] <- 5
+data_italy$transition_2014 <- data_italy$cat_2014
+data_italy$transition_2014[which(data_italy$transition_2014==0)] <- 5
+data_italy$transition_2015 <- data_italy$cat_2015
+data_italy$transition_2015[which(data_italy$transition_2015==0)] <- 5
+data_italy$transition_2016 <- data_italy$cat_2016
+data_italy$transition_2016[which(data_italy$transition_2016==0)] <- 5
+data_italy$transition_2017 <- data_italy$cat_2017
+data_italy$transition_2017[which(data_italy$transition_2017==0)] <- 5
 
-trn_mtrx <-
+trn_mtrx_2010 <-
+  with(data,
+       table(data_italy$transition_2009, 
+             data_italy$transition_2010))
+
+trn_mtrx_2011 <-
   with(data,
        table(data_italy$transition_2010, 
              data_italy$transition_2011))
 
-# 9th decile: 0.59% remain in the same risk category
-# 8th decile: 0.35% remain in the same risk category
-# 7th decile: 0.28% remain in the same risk category
-# 6th decile: 0.17% remain in the same risk category
+trn_mtrx_2012 <-
+  with(data,
+       table(data_italy$transition_2011, 
+             data_italy$transition_2012))
+
+trn_mtrx_2013 <-
+  with(data,
+       table(data_italy$transition_2012, 
+             data_italy$transition_2013))
+
+trn_mtrx_2014 <-
+  with(data,
+       table(data_italy$transition_2013, 
+             data_italy$transition_2014))
+
+trn_mtrx_2015 <-
+  with(data,
+       table(data_italy$transition_2014, 
+             data_italy$transition_2015))
+
+trn_mtrx_2016 <-
+  with(data,
+       table(data_italy$transition_2015, 
+             data_italy$transition_2016))
+
+trn_mtrx_2017 <-
+  with(data,
+       table(data_italy$transition_2016, 
+             data_italy$transition_2017))
+
+trn_mtrx <- trn_mtrx_2010 + trn_mtrx_2011 + trn_mtrx_2012 + trn_mtrx_2013 + trn_mtrx_2014 + trn_mtrx_2015 + trn_mtrx_2016 + trn_mtrx_2017
+prop.table(trn_mtrx, 2)
+
+# 9th decile: 0.62% remain in the same risk category
+# 8th decile: 0.39% remain in the same risk category
+# 7th decile: 0.29% remain in the same risk category
+# 6th decile: 0.30% remain in the same risk category
 
 # Sankey Plot
 output_perc <- function(txt) sprintf("%s", txt)
