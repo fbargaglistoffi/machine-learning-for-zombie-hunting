1ff40f76ede534931a7b40e4582a8e368e80a82b
Fabio Incerti <fabio.incerti@imtlucca.it>
2023-01-12 19:28:59 +0100

    New 5-folds CV code + functions

--- R_code/functions/functions.R
+++ R_code/functions/functions.R
@@ -127,3 +127,78 @@ DtD <- function(mcap, debt, vol, r){
   # Return DtD
   return(e)
 }
+
+
+
+
+
+
+
+########################### Correlation table when many columns are available
+corr_simple <- function(data, sig){
+  #convert data to numeric in order to run correlations
+  #convert to factor first to keep the integrity of the data - each value will become a number rather than turn into NA
+  df_cor <- data %>% mutate_if(is.character, as.factor)
+  df_cor <- df_cor %>% mutate_if(is.factor, as.numeric)
+  #run a correlation and drop the insignificant ones
+  corr <- cor(df_cor)
+  #prepare to drop duplicates and correlations of 1     
+  corr[lower.tri(corr,diag=TRUE)] <- NA 
+  #drop perfect correlations
+  corr[corr == 1] <- NA 
+  #turn into a 3-column table
+  corr <- as.data.frame(as.table(corr))
+  #remove the NA values from above 
+  corr <- na.omit(corr) 
+  #select significant values  
+  corr <- subset(corr, abs(Freq) > sig) 
+  #sort by highest correlation
+  corr <- corr[order(-abs(corr$Freq)),] 
+  #print table
+  print(corr)
+  #turn corr back into matrix in order to plot with corrplot
+  mtx_corr <- reshape2::acast(corr, Var1~Var2, value.var="Freq")
+  
+  #plot correlations visually
+  corrplot(mtx_corr, is.corr=FALSE, tl.col="black", na.label=" ")
+}
+
+
+
+
+
+############################
+SL.bartMachine.better <- function(Y, X, newX, family, obsWeights, id,
+                           num_trees = 50, num_burn_in = 250, verbose = TRUE,
+                           alpha = 0.95, beta = 2, k = 2, q = 0.9, nu = 3,
+                           num_iterations_after_burn_in = 1000, ...) {
+#.SL.require("bartMachine")
+  model = bartMachine::bartMachine(X, Y, num_trees = 50,
+                                   num_burn_in = 250, verbose = TRUE,
+                                   alpha = 0.95, beta = 2, k = 2, q = 0.9, nu = 3,
+                                   num_iterations_after_burn_in = 1000)
+  # pred returns predicted responses (on the scale of the outcome)
+  pred <- predict(model, newX)
+  # fit returns all objects needed for predict.SL.template
+  fit <- list(object = model)
+  #fit <- vector("list", length=0)
+  class(fit) <- c("SL.bartMachine")
+  out <- list(pred = pred, fit = fit)
+  return(out)
+}
+
+#' bartMachine prediction
+#' @param object SuperLearner object
+#' @param newdata Dataframe to predict the outcome
+#' @param family "gaussian" for regression, "binomial" for binary
+#'   classification. (Not used)
+#' @param Y Outcome variable (not used)
+#' @param X Covariate dataframe (not used)
+#' @param ... Additional arguments (not used)
+#'
+#' @export
+predict.SL.bartMachine <- function(object, newdata, family, X = NULL, Y = NULL,...) {
+  .SL.require("bartMachine")
+  pred <- predict(object$object, newdata)
+  return(pred)
+}
